0d325acce0681cdd596c539b30f03915416ac786
roldanazp <c0ng0r3d>
2019-03-31 21:17:46 -0600

    [REVIEW'S CORRECTIONS]  banner_ad_unit_id move to free strings and asynctask tested without initialize activity

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleInstrumentedTest.java
@@ -1,20 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertNotNull;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isEmptyString;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -24,13 +18,14 @@ import static org.hamcrest.CoreMatchers.not;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule =
-            new ActivityTestRule<>(MainActivity.class);
-
     @Test
     public void useAppContext() {
-        onView(ViewMatchers.withId(R.id.b_joke)).perform(click());
-        onView(ViewMatchers.withId(R.id.joke)).check(matches(not(withText(""))));
+        new EndpointsAsyncTask().execute(new EndpointsAsyncTask.Callback() {
+            @Override
+            public void resultJoke(String joke) {
+                assertNotNull(joke);
+                assertThat(joke, not(isEmptyString()));
+            }
+        });
     }
 }
